77bbd32f1f5cc4fb9656e4c97ee782599effee12
ssmaiml <515493749@qq.com>
2018-07-19 15:00:41 +0800

    画好九宫格

--- App.js
+++ App.js
@@ -4,91 +4,97 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
 
-class Square extends Component{
+var color_black = '#000000';
+var color_white = '#ffffff';
 
-  render(){
-    return(
-    <TouchableWithoutFeedback 
-    onPress={this.props.onClick}
-    >
-      <View>
-        <Text>{this.props.value}</Text>
-      </View>
-    </TouchableWithoutFeedback>
-    );
-  }
+class Square extends Component {
+
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={this.props.onClick}>
+                <View>
+                    <Text style={styles.square}>{this.props.value}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
-class Board extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill('Y'),
-    };
-  }
-  
-  handleClick(i){
-    var current = this.state;
-    current.squares[i] = 'X';
-    this.setState(current);
-  }
+class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill('Y')
+        };
+    }
 
-  renderSquare(i){
-    return(
-      <Square 
-      value={this.state.squares[i]} 
-      onClick = {()=> this.handleClick(i)}
-      />
-    );
-  }
+    handleClick(i) {
+        var current = this.state;
+        current.squares[i] = 'X';
+        this.setState(current);
+    }
 
+    renderSquare(i) {
+        return (<Square value={this.state.squares[i]} onClick= {()=> this.handleClick(i)}/>);
+    }
 
-  render(){
-    return(
-      <View>
-        {/* renderSquare(0); */}
-        {/* <Square value={this.state.squares[0]}/> */}
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style = {styles.board}>
+              <View style = {styles.row}>
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+              </View>
+              <View style = {styles.row}>
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+              </View>
+              <View style = {styles.row}>
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+              </View>
+            </View>
+        );
+    }
 }
-
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <View>
-        <Text>
-          dfsafdasfad
-        </Text>
-        <Board/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Board/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-
+    square: {
+      backgroundColor: color_white,
+      textAlign: 'center',
+      textAlignVertical: 'center',
+      width: 50,
+      height: 50
+    },
+    board:{
+      backgroundColor: color_black,
+      width:156,
+      height:156,
+      flexDirection: 'column',
+      justifyContent: 'space-evenly'
+    },
+    row:{
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+    },
 });
